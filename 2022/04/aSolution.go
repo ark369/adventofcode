e237355941e325c5db4117b32cfec2d8a16ef3e5
ark369 <ark369@gmail.com>
2023-12-06 22:27:25 -0500

    Create aSolution.go

---
+++ 2022/04/aSolution.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func Atoi(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+type TwoJobs struct {
+	a1, a2, b1, b2 int
+}
+
+func MakeTwoJobs(s string) *TwoJobs {
+	parts := strings.Split(s, ",")
+	a := strings.Split(parts[0], "-")
+	b := strings.Split(parts[1], "-")
+	return &TwoJobs{Atoi(a[0]), Atoi(a[1]), Atoi(b[0]), Atoi(b[1])}
+}
+
+func (t *TwoJobs) FullyContained() bool {
+	if t.a1 >= t.b1 && t.a2 <= t.b2 {
+		return true
+	}
+	if t.a1 <= t.b1 && t.a2 >= t.b2 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	//input := ReadInput()
+	input := ReadFakeInput()
+
+	sum := 0
+	for _, l := range input {
+		t := MakeTwoJobs(l)
+		if t.FullyContained() {
+			sum++
+		}
+	}
+
+	fmt.Printf("sum: %d\n", sum)
+}
+
+func ReadFakeInput() []string {
+	input := `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
+	return strings.Split(input, "\n")
+}
+
+func ReadInput() []string {
+	input := ``
+	return strings.Split(input, "\n")
+}
